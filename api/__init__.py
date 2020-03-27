10920b597d4c8fc3a981aa212f7929b1ebd20baf
Arjen Dijksta <dijkstra.arjen@gmail.com>
2020-03-27 14:46:23 +0100

    remove unused code

--- api/__init__.py
+++ api/__init__.py
@@ -1,24 +1,13 @@
 import logging
 
-from flask import jsonify
 from flask_restful import Api, Resource
 
 import config
 import tasks
 
-
 logger = logging.getLogger()
 
 api = Api(prefix=config.API_PREFIX)
-
-
-class TaskStatusAPI(Resource):
-    def get(self, task_id):
-        task = tasks.celery.AsyncResult(task_id)
-        logger.info("TaskStatusAPI")
-        logger.info("Task: %s", task)
-
-        return jsonify(task.result)
 
 
 class DataProcessingAPI(Resource):
@@ -27,10 +16,8 @@ class DataProcessingAPI(Resource):
         task = tasks.process_data.delay()
 
         logger.info("Task: %s", task)
+
         return {'task_id': task.id}, 200
 
 # data processing endpoint
 api.add_resource(DataProcessingAPI, '/process_data')
-
-# task status endpoint
-api.add_resource(TaskStatusAPI, '/tasks/<string:task_id>')
