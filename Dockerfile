112ebb4f81dcbf8979a9709af7ec49d862e8c1a1
Arjen Dijksta <dijkstra.arjen@gmail.com>
2020-03-29 22:14:15 +0200

    added flower to docker

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,10 @@ WORKDIR /flask-deploy
 RUN pipenv install --system --skip-lock
 
 RUN pip install gunicorn[gevent]
+RUN pip install flower
 
 EXPOSE 8000
 
-CMD gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:8000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
+# CMD gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:8000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
+
+CMD python app.py
--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,8 @@
 import logging
+import time
 
 from flask_restful import Api, Resource
+from flask import jsonify
 
 import config
 import tasks
@@ -10,8 +12,36 @@ logger = logging.getLogger()
 api = Api(prefix=config.API_PREFIX)
 
 
+class TaskStatusAPI(Resource):
+    @staticmethod
+    def get(task_id):
+        task = tasks.celery.AsyncResult(task_id)
+        logger.info("TaskStatusAPI")
+        logger.info("Task: %s", task)
+        if task.state == 'PENDING':
+            time.sleep(1)
+            response = {
+                'queue_state': task.state,
+                'status': 'Process is ongoing...',
+            }
+        elif task.state == 'SUCCESS':
+
+            response = {
+                'queue_state': task.state,
+                'result': task.wait()
+            }
+        else:
+            response = {
+                'queue_state': task.state,
+                'result': task.wait()
+            }
+
+        return jsonify(response)
+
+
 class DataProcessingAPI(Resource):
-    def post(self):
+    @staticmethod
+    def post():
         logger.info("DataProcessingAPI")
         task = tasks.process_data.delay()
 
@@ -21,3 +51,5 @@ class DataProcessingAPI(Resource):
 
 # data processing endpoint
 api.add_resource(DataProcessingAPI, '/process_data')
+# task status endpoint
+api.add_resource(TaskStatusAPI, '/tasks/<string:task_id>')
